a37123d79176bb770326f2a2692cc332d2d7484c
Anuchitra <anujayachitra99@gmail.com>
2022-10-12 23:17:13 +0530

    First commit

---
+++ Phase5_Project/Practice_Project_2/awsspringboot/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MainController {
+	
+	@GetMapping("/")
+	public String Hello()
+	{
+	  return "Welcome to deployment of spring boot project...";
+	  }
+
+}
